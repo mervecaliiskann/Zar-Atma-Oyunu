8b8db58a26672f2407f4470930bdccaa88a137cf
MerveÇalışkan <mervecaliiskannk@gmail.com>
2024-06-04 18:42:43 +0300

    Arka Plan Resmi
    Arka plan resmini kod yardımıyla, assets dosyasına yüklediğimiz color ile değiştirdik.

--- Zar Atma Oyunu/Zar Atma Oyunu/ViewController.swift
+++ Zar Atma Oyunu/Zar Atma Oyunu/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblOyuncu1Skor: UILabel!
+    @IBOutlet weak var lblOyuncu2Skor: UILabel!
+    @IBOutlet weak var lblOyuncu1Puan: UILabel!
+    @IBOutlet weak var lblOyuncu2Puan: UILabel!
+    @IBOutlet weak var imgOyuncu1Durum: UIImageView!
+    @IBOutlet weak var imgOyuncu2Durum: UIImageView!
+    @IBOutlet weak var imgZar1: UIImageView!
+    @IBOutlet weak var imgZar2: UIImageView!
+    @IBOutlet weak var lblSetSonucu: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        Arka plana görüntü aktarmak için;
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "arkaPlan")!)
+       
     }
 
 
